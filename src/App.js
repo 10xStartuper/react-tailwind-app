0005506cc8d3e28674fb7336d447c100a58ce274
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-08-18 11:27:37 +0500

    DropDown toggle added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState, useEffect, useCallback } from "react";
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appintmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
+
 function App() {
+  const [appointmentList, setAppointmentList] = useState([]);
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setAppointmentList(data));
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <div className="container mx-auto mt-5 font-thin">
       <h2 className="text-5xl mb-5">
@@ -13,7 +24,7 @@ function App() {
       <AddAppointment />
       <Search />
       <ul className="divide-y divide-gray-200">
-        {appintmentList.map((appointment) => {
+        {appointmentList.map((appointment) => {
           return (
             <AppointmentInfo key={appointment.id} appointment={appointment} />
           );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,9 @@
-import React from "react";
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
-const DropDown = () => {
+import { useState } from "react";
+const DropDown = ({ toggle }) => {
+  if (!toggle) {
+    return null;
+  }
   return (
     <div
       className="origin-top-right absolute right-0 mt-2 w-56
@@ -47,6 +50,7 @@ const DropDown = () => {
   );
 };
 const Search = () => {
+  const [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -65,6 +69,9 @@ const Search = () => {
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
             <button
+              onClick={() => {
+                setToggleSort(!toggleSort);
+              }}
               type="button"
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
               id="options-menu"
@@ -73,7 +80,7 @@ const Search = () => {
             >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown />
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
